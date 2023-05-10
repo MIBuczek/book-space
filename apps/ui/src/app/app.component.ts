612b88f4f846fe8ecacab91c3878c5ca8988df57
MIBuczek <michalbuczek@hotmail.com>
2023-05-10 22:01:49 +0200

    rename the project

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 @Component({
   standalone: true,
   imports: [NxWelcomeComponent, RouterModule],
-  selector: 'my-links-root',
+  selector: 'book-space-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
